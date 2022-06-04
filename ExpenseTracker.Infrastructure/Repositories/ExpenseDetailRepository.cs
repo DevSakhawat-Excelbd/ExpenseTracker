cb5aa15ecf6583a677eb533380b1e57bff5c7f85
shakil <shakil.dev@excelbd.com>
2022-06-04 14:31:24 +0600

    add expense repo

---
+++ ExpenseTracker.Infrastructure/Repositories/ExpenseDetailRepository.cs
@@ -0,0 +1,16 @@
+﻿using ExpenseTracker.Domain.Entities;
+using ExpenseTracker.Infrastructure.Contracts;
+using ExpenseTracker.Infrastructure.Sql;
+
+namespace ExpenseTracker.Infrastructure.Repositories
+{
+    public class ExpenseDetailRepository : Repository<ExpenseDetail>, IExpenseDetailRepository
+    {
+        private readonly DataContext context;
+        public ExpenseDetailRepository(DataContext context) : base(context)
+        {
+            this.context = context;
+        }
+
+    }
+}
