b47fd33dd645b5ff0d4a006a791e796b01301645
Sakhawat Hossain <devsakhawat@outlook.com>
2022-06-04 15:03:24 +0600

    ICategoryRepository and CategoryRepository

---
+++ ExpenseTracker.Infrastructure/Repositories/CategoryRepositoy.cs
@@ -0,0 +1,20 @@
+﻿using ExpenseTracker.Domain.Entities;
+using ExpenseTracker.Infrastructure.Contracts;
+using ExpenseTracker.Infrastructure.Sql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Infrastructure.Repositories
+{
+    public class CategoryRepositoy : Repository<Category>, ICategoryRepositoy
+    {
+        private readonly DataContext context;
+        public CategoryRepositoy(DataContext context) : base(context)
+        {
+            this.context = context;
+        }
+    }
+}
