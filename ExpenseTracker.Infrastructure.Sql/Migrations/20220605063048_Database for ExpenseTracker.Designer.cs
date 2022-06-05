2e2a5da20690bf7baf63c7ed38ce3146845d936e
Sakhawat Hossain <devsakhawat@outlook.com>
2022-06-05 13:11:23 +0600

    add category api controller

--- ExpenseTracker.Infrastructure.Sql/Migrations/20220605063048_Database for ExpenseTracker.Designer.cs
+++ ExpenseTracker.Infrastructure.Sql/Migrations/20220605063048_Database for ExpenseTracker.Designer.cs
@@ -4,6 +4,7 @@ using ExpenseTracker.Infrastructure.Sql;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExpenseTracker.Infrastructure.Sql.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220605063048_Database for ExpenseTracker")]
+    partial class DatabaseforExpenseTracker
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
